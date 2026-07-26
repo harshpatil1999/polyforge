bc5da71e918bbf41f2f03ddf6eef038231602cfb
harshpatil1999 <patilh652@gmail.com>
2026-07-26 17:46:33 +0530

    implement session management using redis

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { getAuth } from "firebase-admin/auth";
 import User from "../models/user.model.js";
 import { app } from "../config/firebase.js";
+import redis from "../../../shared/redis/redis.js";
 
 export const loginController = async (req, res) => {
   try {
@@ -18,6 +19,17 @@ export const loginController = async (req, res) => {
       });
     }
     const sessionId = crypto.randomUUID;
+    await redis.set(
+      `session-${sessionId}`,
+      JSON.stringify({
+        userId: user._id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+      }),
+      "EX",
+      7 * 24 * 60 * 60,
+    );
     res.cookie("session", sessionId, {
       httpOnly: true,
       secure: false,
@@ -28,6 +40,19 @@ export const loginController = async (req, res) => {
   } catch (error) {
     return res
       .status(500)
-      .json({ message: "Error loggin in : " + error.message });
+      .json({ message: "Error logging in : " + error.message });
+  }
+};
+
+export const logoutController = async (req, res) => {
+  try {
+    const sessionId = req.cookies?.session;
+    await redis.del(`session-${sessionId}`);
+    res.clearCookie("session");
+    return res.status(200).json({ message: "Successfully logged out!" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Error logging out : " + error.message });
   }
 };
--- backend/services/auth/routes/auth.routes.js
+++ backend/services/auth/routes/auth.routes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { loginController } from "../controllers/auth.controller.js";
+import {
+  loginController,
+  logoutController,
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/login", loginController);
+router.get("/logout", logoutController);
 
 export default router;
